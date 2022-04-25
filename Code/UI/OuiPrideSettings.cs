b48171d06bba94f84e80cff1ca3f76b028cb84fe
catapillie <espritox@gmail.com>
2022-04-25 04:02:47 +0200

    Subtle reformatting in OuiPrideSettings.cs

--- Code/UI/OuiPrideSettings.cs
+++ Code/UI/OuiPrideSettings.cs
@@ -10,37 +10,37 @@ namespace Celeste.Mod.PrideMod.UI {
             bool collabUtils2Loaded = Dependencies.CollabUtils2_Loaded;
 
 
-            CreateSubHeader     (menu, "modoptions_PrideMod_header_CrystalHearts");
-            CreatePrideSetting  (menu, "modoptions_PrideMod_ASideCrystalHeart", settings.ASideCrystalHeart, prideType => settings.ASideCrystalHeart = prideType, displayDesc: true);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_BSideCrystalHeart", settings.BSideCrystalHeart, prideType => settings.BSideCrystalHeart = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_CSideCrystalHeart", settings.CSideCrystalHeart, prideType => settings.CSideCrystalHeart = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_EmptyCrystalHeart", settings.EmptyCrystalHeart, prideType => settings.EmptyCrystalHeart = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_GhostCrystalHeart", settings.GhostCrystalHeart, prideType => settings.GhostCrystalHeart = prideType);
+            CreateSubHeader(menu, "modoptions_PrideMod_header_CrystalHearts");
+            CreatePrideSetting(menu, "modoptions_PrideMod_ASideCrystalHeart", settings.ASideCrystalHeart, prideType => settings.ASideCrystalHeart = prideType, displayDesc: true);
+            CreatePrideSetting(menu, "modoptions_PrideMod_BSideCrystalHeart", settings.BSideCrystalHeart, prideType => settings.BSideCrystalHeart = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_CSideCrystalHeart", settings.CSideCrystalHeart, prideType => settings.CSideCrystalHeart = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_EmptyCrystalHeart", settings.EmptyCrystalHeart, prideType => settings.EmptyCrystalHeart = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GhostCrystalHeart", settings.GhostCrystalHeart, prideType => settings.GhostCrystalHeart = prideType);
 
             if (collabUtils2Loaded) {
-                CreateSubHeader     (menu, "modoptions_PrideMod_header_MiniHearts");
-                CreatePrideSetting  (menu, "modoptions_PrideMod_BeginnerMiniHeart", settings.BeginnerMiniHeart, prideType => settings.BeginnerMiniHeart = prideType);
-                CreatePrideSetting  (menu, "modoptions_PrideMod_IntermediateMiniHeart", settings.IntermediateMiniHeart, prideType => settings.IntermediateMiniHeart = prideType);
-                CreatePrideSetting  (menu, "modoptions_PrideMod_AdvancedMiniHeart", settings.AdvancedMiniHeart, prideType => settings.AdvancedMiniHeart = prideType);
-                CreatePrideSetting  (menu, "modoptions_PrideMod_ExpertMiniHeart", settings.ExpertMiniHeart, prideType => settings.ExpertMiniHeart = prideType);
-                CreatePrideSetting  (menu, "modoptions_PrideMod_GrandmasterMiniHeart", settings.GrandmasterMiniHeart, prideType => settings.GrandmasterMiniHeart = prideType);
-                CreatePrideSetting  (menu, "modoptions_PrideMod_GhostMiniHeart", settings.GhostMiniHeart, prideType => settings.GhostMiniHeart = prideType);
+                CreateSubHeader(menu, "modoptions_PrideMod_header_MiniHearts");
+                CreatePrideSetting(menu, "modoptions_PrideMod_BeginnerMiniHeart", settings.BeginnerMiniHeart, prideType => settings.BeginnerMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_IntermediateMiniHeart", settings.IntermediateMiniHeart, prideType => settings.IntermediateMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_AdvancedMiniHeart", settings.AdvancedMiniHeart, prideType => settings.AdvancedMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_ExpertMiniHeart", settings.ExpertMiniHeart, prideType => settings.ExpertMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_GrandmasterMiniHeart", settings.GrandmasterMiniHeart, prideType => settings.GrandmasterMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_GhostMiniHeart", settings.GhostMiniHeart, prideType => settings.GhostMiniHeart = prideType);
             }
 
-            CreateSubHeader     (menu, "modoptions_PrideMod_header_Strawberries");
-            CreatePrideSetting  (menu, "modoptions_PrideMod_Strawberry", settings.Strawberry, prideType => settings.Strawberry = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_GhostStrawberry", settings.GhostStrawberry, prideType => settings.GhostStrawberry = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_GoldenStrawberry", settings.GoldenStrawberry, prideType => settings.GoldenStrawberry = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_GhostGoldenStrawberry", settings.GhostGoldenStrawberry, prideType => settings.GhostGoldenStrawberry = prideType);
+            CreateSubHeader(menu, "modoptions_PrideMod_header_Strawberries");
+            CreatePrideSetting(menu, "modoptions_PrideMod_Strawberry", settings.Strawberry, prideType => settings.Strawberry = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GhostStrawberry", settings.GhostStrawberry, prideType => settings.GhostStrawberry = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GoldenStrawberry", settings.GoldenStrawberry, prideType => settings.GoldenStrawberry = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GhostGoldenStrawberry", settings.GhostGoldenStrawberry, prideType => settings.GhostGoldenStrawberry = prideType);
             if (collabUtils2Loaded) {
                 CreatePrideSetting(menu, "modoptions_PrideMod_SilverStrawberry", settings.SilverStrawberry, prideType => settings.SilverStrawberry = prideType);
                 CreatePrideSetting(menu, "modoptions_PrideMod_GhostSilverStrawberry", settings.GhostSilverStrawberry, prideType => settings.GhostSilverStrawberry = prideType);
             }
 
-            CreateSubHeader     (menu, "modoptions_PrideMod_header_Flags");
-            CreatePrideSetting  (menu, "modoptions_PrideMod_SummitFlag", settings.SummitFlag, prideType => settings.SummitFlag = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_FinalFlag", settings.FinalFlag, prideType => settings.FinalFlag = prideType);
-            CreatePrideSetting  (menu, "modoptions_PrideMod_Confetti", settings.Confetti, prideType => settings.Confetti = prideType);
+            CreateSubHeader(menu, "modoptions_PrideMod_header_Flags");
+            CreatePrideSetting(menu, "modoptions_PrideMod_SummitFlag", settings.SummitFlag, prideType => settings.SummitFlag = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_FinalFlag", settings.FinalFlag, prideType => settings.FinalFlag = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_Confetti", settings.Confetti, prideType => settings.Confetti = prideType);
         }
 
         private void CreatePrideSetting(TextMenu menu, string settingName, PrideTypes value, Action<PrideTypes> action, bool displayDesc = false) {
