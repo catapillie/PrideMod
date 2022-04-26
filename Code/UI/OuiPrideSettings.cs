5c7cda118ea092dfebc844f61e3007cf5aead0db
catapillie <espritox@gmail.com>
2022-04-26 02:40:07 +0200

    Create and display sprites for pride settings

--- Code/UI/OuiPrideSettings.cs
+++ Code/UI/OuiPrideSettings.cs
@@ -11,45 +11,46 @@ namespace Celeste.Mod.PrideMod.UI {
 
 
             CreateSubHeader(menu, "modoptions_PrideMod_header_CrystalHearts");
-            CreatePrideSetting(menu, "modoptions_PrideMod_ASideCrystalHeart", settings.ASideCrystalHeart, prideType => settings.ASideCrystalHeart = prideType, displayDesc: true);
-            CreatePrideSetting(menu, "modoptions_PrideMod_BSideCrystalHeart", settings.BSideCrystalHeart, prideType => settings.BSideCrystalHeart = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_CSideCrystalHeart", settings.CSideCrystalHeart, prideType => settings.CSideCrystalHeart = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_EmptyCrystalHeart", settings.EmptyCrystalHeart, prideType => settings.EmptyCrystalHeart = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_GhostCrystalHeart", settings.GhostCrystalHeart, prideType => settings.GhostCrystalHeart = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_ASideCrystalHeart", "crystalheart", settings.ASideCrystalHeart, prideType => settings.ASideCrystalHeart = prideType, displayDesc: true);
+            CreatePrideSetting(menu, "modoptions_PrideMod_BSideCrystalHeart", "crystalheart", settings.BSideCrystalHeart, prideType => settings.BSideCrystalHeart = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_CSideCrystalHeart", "crystalheart", settings.CSideCrystalHeart, prideType => settings.CSideCrystalHeart = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_EmptyCrystalHeart", "crystalheart", settings.EmptyCrystalHeart, prideType => settings.EmptyCrystalHeart = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GhostCrystalHeart", "crystalheart", settings.GhostCrystalHeart, prideType => settings.GhostCrystalHeart = prideType);
 
             if (collabUtils2Loaded) {
                 CreateSubHeader(menu, "modoptions_PrideMod_header_MiniHearts");
-                CreatePrideSetting(menu, "modoptions_PrideMod_BeginnerMiniHeart", settings.BeginnerMiniHeart, prideType => settings.BeginnerMiniHeart = prideType);
-                CreatePrideSetting(menu, "modoptions_PrideMod_IntermediateMiniHeart", settings.IntermediateMiniHeart, prideType => settings.IntermediateMiniHeart = prideType);
-                CreatePrideSetting(menu, "modoptions_PrideMod_AdvancedMiniHeart", settings.AdvancedMiniHeart, prideType => settings.AdvancedMiniHeart = prideType);
-                CreatePrideSetting(menu, "modoptions_PrideMod_ExpertMiniHeart", settings.ExpertMiniHeart, prideType => settings.ExpertMiniHeart = prideType);
-                CreatePrideSetting(menu, "modoptions_PrideMod_GrandmasterMiniHeart", settings.GrandmasterMiniHeart, prideType => settings.GrandmasterMiniHeart = prideType);
-                CreatePrideSetting(menu, "modoptions_PrideMod_GhostMiniHeart", settings.GhostMiniHeart, prideType => settings.GhostMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_BeginnerMiniHeart", "miniheart", settings.BeginnerMiniHeart, prideType => settings.BeginnerMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_IntermediateMiniHeart", "miniheart", settings.IntermediateMiniHeart, prideType => settings.IntermediateMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_AdvancedMiniHeart", "miniheart", settings.AdvancedMiniHeart, prideType => settings.AdvancedMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_ExpertMiniHeart", "miniheart", settings.ExpertMiniHeart, prideType => settings.ExpertMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_GrandmasterMiniHeart", "miniheart", settings.GrandmasterMiniHeart, prideType => settings.GrandmasterMiniHeart = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_GhostMiniHeart", "miniheart", settings.GhostMiniHeart, prideType => settings.GhostMiniHeart = prideType);
             }
 
             CreateSubHeader(menu, "modoptions_PrideMod_header_Strawberries");
-            CreatePrideSetting(menu, "modoptions_PrideMod_Strawberry", settings.Strawberry, prideType => settings.Strawberry = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_GhostStrawberry", settings.GhostStrawberry, prideType => settings.GhostStrawberry = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_GoldenStrawberry", settings.GoldenStrawberry, prideType => settings.GoldenStrawberry = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_GhostGoldenStrawberry", settings.GhostGoldenStrawberry, prideType => settings.GhostGoldenStrawberry = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_Strawberry", "strawberry", settings.Strawberry, prideType => settings.Strawberry = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GhostStrawberry", "ghostberry", settings.GhostStrawberry, prideType => settings.GhostStrawberry = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GoldenStrawberry", "goldenberry", settings.GoldenStrawberry, prideType => settings.GoldenStrawberry = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_GhostGoldenStrawberry", "ghostgoldenberry", settings.GhostGoldenStrawberry, prideType => settings.GhostGoldenStrawberry = prideType);
             if (collabUtils2Loaded) {
-                CreatePrideSetting(menu, "modoptions_PrideMod_SilverStrawberry", settings.SilverStrawberry, prideType => settings.SilverStrawberry = prideType);
-                CreatePrideSetting(menu, "modoptions_PrideMod_GhostSilverStrawberry", settings.GhostSilverStrawberry, prideType => settings.GhostSilverStrawberry = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_SilverStrawberry", "silverberry", settings.SilverStrawberry, prideType => settings.SilverStrawberry = prideType);
+                CreatePrideSetting(menu, "modoptions_PrideMod_GhostSilverStrawberry", "ghostsilverberry", settings.GhostSilverStrawberry, prideType => settings.GhostSilverStrawberry = prideType);
             }
 
             CreateSubHeader(menu, "modoptions_PrideMod_header_Flags");
-            CreatePrideSetting(menu, "modoptions_PrideMod_SummitFlag", settings.SummitFlag, prideType => settings.SummitFlag = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_FinalFlag", settings.FinalFlag, prideType => settings.FinalFlag = prideType);
-            CreatePrideSetting(menu, "modoptions_PrideMod_Confetti", settings.Confetti, prideType => settings.Confetti = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_SummitFlag", "...", settings.SummitFlag, prideType => settings.SummitFlag = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_FinalFlag", "...", settings.FinalFlag, prideType => settings.FinalFlag = prideType);
+            CreatePrideSetting(menu, "modoptions_PrideMod_Confetti", "...", settings.Confetti, prideType => settings.Confetti = prideType);
         }
 
-        private void CreatePrideSetting(TextMenu menu, string settingName, PrideTypes value, Action<PrideTypes> action, bool displayDesc = false) {
-            TextMenu.Item item = new PreviewedSlider(
+        private void CreatePrideSetting(TextMenu menu, string settingName, string spriteType, PrideTypes value, Action<PrideTypes> action, bool displayDesc = false) {
+            PrideSlider item = new PrideSlider(
                 Dialog.Clean(settingName),
-                i => ((PrideTypes)i).GetFormattedName(),
+                spriteType,
+                i => i.GetFormattedName(),
                 0, PrideData.PrideCount - 1,
                 (int)value
-            ).Change(i => action((PrideTypes)i));
+            ).Change(i => action((PrideTypes)i)) as PrideSlider;
 
             menu.Add(item);
             item.AddDescription(menu, Dialog.Clean(settingName + "_sub"));
---
+++ Code/UI/PrideSlider.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Monocle;
+using System;
+
+namespace Celeste.Mod.PrideMod.UI {
+    public class PrideSlider : TextMenu.Option<int> {
+        public PrideTypes PrideType {
+            get => (PrideTypes)Index;
+            set => Index = (int)value;
+        }
+
+        private readonly string spriteType;
+        private Sprite sprite;
+
+        public PrideSlider(string label, string spriteType, Func<PrideTypes, string> values, int min, int max, int value = -1)
+            : base(label) {
+            for (int i = min; i <= max; i++)
+                Add(values((PrideTypes)i), i, value == i);
+
+            this.spriteType = spriteType;
+            RecreatePreviewSprite();
+        }
+
+        public override void LeftPressed() {
+            int prev = Index;
+            base.LeftPressed();
+            if (Index != prev)
+                RecreatePreviewSprite();
+        }
+
+        public override void RightPressed() {
+            int prev = Index;
+            base.RightPressed();
+            if (Index != prev)
+                RecreatePreviewSprite();
+        }
+
+        private void RecreatePreviewSprite() {
+            if (spriteType != "...") {
+                sprite = GFX.SpriteBank.Create(PrideType.GetCustomSpriteID(spriteType, "strawberry"));
+                sprite.Play("idle");
+            }
+        }
+
+        public override void Update() {
+            base.Update();
+
+            if (Container.Current == this && Container.Focused)
+                sprite?.Update();
+        }
+
+        public override void Render(Vector2 position, bool highlighted) {
+            base.Render(position, highlighted);
+
+            if (highlighted) {
+                Draw.SpriteBatch.End();
+                SamplerState oldSamplerState = Draw.SpriteBatch.GraphicsDevice.SamplerStates[0];
+
+                Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, DepthStencilState.None, RasterizerState.CullNone);
+
+                if (sprite != null && sprite.Texture != null) {
+                    sprite.Texture.DrawCentered(position + new Vector2(Container.Width + 100, 0), Color.White, 6f);
+                }
+
+                Draw.SpriteBatch.End();
+
+                Draw.SpriteBatch.GraphicsDevice.SamplerStates[0] = oldSamplerState;
+                Draw.SpriteBatch.Begin();
+            }
+        }
+    }
+}
