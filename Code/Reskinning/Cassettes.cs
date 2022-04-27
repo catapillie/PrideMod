cc302e5917610e26df3f099a560c15f0d450524c
catapillie <espritox@gmail.com>
2022-04-27 09:06:08 +0200

    Apply minimal bloom alpha value on Cassettes

--- Code/Reskinning/Cassettes.cs
+++ Code/Reskinning/Cassettes.cs
@@ -1,10 +1,14 @@
-﻿using MonoMod.Cil;
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.Utils;
 using System;
 
 namespace Celeste.Mod.PrideMod.Reskinning {
     public static class Cassettes {
         private const string cassette_sprite        = "cassette";
         private const string cassetteghost_sprite   = "cassetteGhost";
+
+        private const string PrideMod_Cassette_wasReskinned = "PrideMod_Cassette_wasReskinned";
 
         internal static void Hook() {
             IL.Celeste.Cassette.Added += Mod_Cassette_Added;
@@ -18,15 +22,48 @@ namespace Celeste.Mod.PrideMod.Reskinning {
             ILCursor cursor = new(il);
 
             cursor.GotoNext(MoveType.After, instr => instr.MatchLdstr(cassette_sprite));
-            cursor.EmitDelegate<Func<string, string>>(id => {
+
+            cursor.Emit(OpCodes.Ldarg_0);
+            cursor.EmitDelegate<Func<string, Cassette, string>>((id, cassette) => {
                 PrideModModuleSettings settings = PrideModModule.Settings;
-                return settings.Enabled ? settings.Cassette.GetCustomSpriteID("cassette", id) : id;
+                if (settings.Enabled) {
+                    if (settings.Cassette != PrideTypes.Default) {
+                        id = settings.Cassette.GetCustomSpriteID("cassette", id);
+                        new DynData<Cassette>(cassette)[PrideMod_Cassette_wasReskinned] = true;
+                    }
+                }
+
+                return id;
             });
 
             cursor.GotoNext(MoveType.After, instr => instr.MatchLdstr(cassetteghost_sprite));
-            cursor.EmitDelegate<Func<string, string>>(id => {
+
+            cursor.Emit(OpCodes.Ldarg_0);
+            cursor.EmitDelegate<Func<string, Cassette, string>>((id, cassette) => {
                 PrideModModuleSettings settings = PrideModModule.Settings;
-                return settings.Enabled ? settings.GhostCassette.GetCustomSpriteID("cassette", id) : id;
+                if (settings.Enabled) {
+                    if (settings.GhostCassette != PrideTypes.Default) {
+                        id = settings.GhostCassette.GetCustomSpriteID("cassette", id);
+                        new DynData<Cassette>(cassette)[PrideMod_Cassette_wasReskinned] = true;
+                    }
+                }
+
+                return id;
+            });
+
+            cursor.GotoNext(instr => instr.MatchNewobj<BloomPoint>());
+            cursor.GotoPrev(MoveType.After, instr => instr.MatchLdcR4(0.25f));
+
+            cursor.Emit(OpCodes.Ldarg_0);
+            cursor.EmitDelegate<Func<float, Cassette, float>>((alpha, cassette) => {
+                PrideModModuleSettings settings = PrideModModule.Settings;
+                if (settings.Enabled && settings.MinimalBloom) {
+                    DynData<Cassette> data = new(cassette);
+                    if (data.Data.TryGetValue(PrideMod_Cassette_wasReskinned, out var value) && (bool)value)
+                        alpha = 0.05f;
+                }
+
+                return alpha;
             });
         }
     }
